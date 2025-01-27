8f8aea6da876227d6984f66a5bc7756050fc6dff
AdminFedora <segor6052@gmail.com>
2025-01-27 15:41:49 +0200

    Generating rundom ip address

--- src/dao/entity/IpAddress.cpp
+++ src/dao/entity/IpAddress.cpp
@@ -4,22 +4,11 @@
 #include "../lib/Database.h"
 
 std::string Database::getIpAddress(){
-    std::string IP1 = "192.168.1.1";
-    std::string IP2 = "192.168.2.2";
-    std::string IP3 = "192.168.3.3";
-
     std::random_device rd;
     std::mt19937 generator(rd());
     std::uniform_int_distribution<int> distribution(1, 3);
     int randomInt = distribution(generator);
-
-    if (randomInt == 1){
-        return IP1;
-    } else if (randomInt == 2){
-        return IP2;
-    } else if (randomInt == 3){
-        return IP3;
-    } else {
-        return "NoIP";
-    }
+    std::string IP = "192.168." + std::to_string(randomInt) + "." + std::to_string(randomInt);
+    
+    return IP;
 }
