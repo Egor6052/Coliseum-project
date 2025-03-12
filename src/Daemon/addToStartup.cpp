edfc2371200836c3489088d530ccb325ec84b4ae
AdminFedora <segor6052@gmail.com>
2025-03-12 15:13:18 +0200

    Add startup function

--- src/Daemon/addToStartup.cpp
+++ src/Daemon/addToStartup.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 #include "../../lib/Daemon.h"
 
-// Adding to autostart
+// sudo systemctl daemon-reload
+
+// sudo systemctl restart SensorDaemon.service
+
+// sudo systemctl status SensorDaemon.service
+
+// Додавання до автозапуску
 void Daemon::addToStartup() {
     if (servicePath.empty()) {
         std::cerr << "Error: servicePath is empty!" << std::endl;
@@ -17,28 +24,37 @@ void Daemon::addToStartup() {
         return;
     }
 
+    // Отримуємо домашній шлях користувача (для підтримки ~)
+    const char* homeDir = getenv("HOME");
+    if (homeDir == nullptr) {
+        std::cerr << "Error: Unable to find home directory!" << std::endl;
+        return;
+    }
+
     serviceFile << "[Unit]\n";
     serviceFile << "Description=SensorDaemon\n";
     serviceFile << "After=network.target\n\n";
+
     serviceFile << "[Service]\n";
-    serviceFile << "ExecStart=/usr/local/bin/SensorDaemon\n";
+    serviceFile << "ExecStart=" + std::string(homeDir) + "/Documents/Coliseum-project/build/start\n";
     serviceFile << "Restart=always\n";
     serviceFile << "User=root\n";
     serviceFile << "WorkingDirectory=/\n\n";
+
     serviceFile << "[Install]\n";
     serviceFile << "WantedBy=multi-user.target\n";
-    
+
     serviceFile.close();
 
-    // Updating systemd
-    int reloadStatus = system("systemctl daemon-reload");
+    // Оновлення systemd
+    int reloadStatus = system("sudo systemctl daemon-reload");
     if (reloadStatus != 0) {
         std::cerr << "Error: Failed to reload systemd daemon!" << std::endl;
         return;
     }
 
-    // Enabling autorun
-    int enableStatus = system("systemctl enable SensorDaemon.service");
+    // Включення автозапуску
+    int enableStatus = system("sudo systemctl enable SensorDaemon.service");
     if (enableStatus != 0) {
         std::cerr << "Error: Failed to enable SensorDaemon service!" << std::endl;
         return;
